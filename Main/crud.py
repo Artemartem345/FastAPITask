4a0cc51c98c3bf8da34dda63d04fabe5785f5be7
Artem <Popovartem3412@gmail.com>
2023-07-23 22:49:41 +0300

    edits

--- Main/crud.py
+++ Main/crud.py
@@ -5,12 +5,10 @@ from schema import *
 # Блюда
 # Получение блюда
 def get_dish(db:Session):
-    if db is None:
-        return []
-    return db
+    return db.query(Dish).all()
 # Получение блюда по айдишнику
-def get_dish_by_id(db:Session, dish_id):
-    return db.query(DishResponse).filter(Dish.id == dish_id).first()
+def get_dish_by_id(db:Session, dish_id:UUID):
+    return db.query(Dish(DishResponse)).filter(Dish.id == dish_id).first()
 # Создание блюда
 def create_dish(db:Session, dish: DishCreate):
     _dish = Dish(title=dish.title,description=dish.description)
@@ -19,12 +17,12 @@ def create_dish(db:Session, dish: DishCreate):
     db.refresh(_dish)
     return _dish
 # Удаление блюда
-def remove_dish(db:Session, dish_id: Dish):
+def remove_dish(db:Session, dish_id: UUID):
     _dish = get_dish_by_id(db=db, dish_id=dish_id)
     db.delete(_dish)
     db.commit()
 # Обновление блюда
-def update_dish(db:Session, dish_id: DishUpdate, title:str, description:str):
+def update_dish(db:Session, dish_id: UUID, title:str, description:str):
     _dish = get_dish_by_id(db=db, dish_id=dish_id)
     _dish.title = title
     _dish.description = description
@@ -37,12 +35,9 @@ def update_dish(db:Session, dish_id: DishUpdate, title:str, description:str):
 # Меню
 
 def get_menu(db:Session):
-    if db is None:
-        return []
-    return db
-
-def get_menu_by_id(db:Session, menu_id: int):
-    return db.query(Menu).filter(menu_id == menu_id).first()
+    return db.query(Menu).all()
+def get_menu_by_id(db:Session, menu_id: UUID):
+    return db.query(Menu).filter(Menu.id == menu_id).first()
 
 def create_menu(db: Session, menu):
     _menu = menu(title=menu.title, description=menu.description)
@@ -51,7 +46,7 @@ def create_menu(db: Session, menu):
     db.refresh(_menu)
     return _menu
 
-def remove_menu(db:Session, menu_id):
+def remove_menu(db:Session, menu_id:UUID):
     _menu = get_menu_by_id(title=menu_id.title, description=menu_id.description)
     db.delete(_menu)
     db.commit()
@@ -68,29 +63,25 @@ def update_menu(db:Session, menu_id:uuid.UUID, title:str, description:str):
 # СабМеню
 
 def get_menu(db:Session):
-    if db is None:
-        return []
-    return db
+    return db.query(Menu).all()
 
-def get_submenu_by_id(db:Session, menu_id: int):
-    return db.query(Menu).filter(SubmenuResponse.id == menu_id).first()
+def get_submenu_by_id(db:Session, menu_id: UUID):
+    return db.query(Submenu).filter(Submenu.id == menu_id).first()
 
 def create_menu(db: Session, menu):
-    _menu = SubmenuCreate(title=menu.title, description=menu.description)
+    _menu = Submenu(**menu)
     db.add(_menu)
     db.commit()
     db.refresh(_menu)
     return _menu
 
-def remove_menu(db:Session, submenu_id):
+def remove_menu(db:Session, submenu_id: UUID):
     _menu = get_submenu_by_id(title=submenu_id.title, description=submenu_id.description)
     db.delete(_menu)
     db.commit()
     
-def update_menu(db:Session, submenu_id:SubmenuUpdate, title:str, description:str):
-    _submenu = get_submenu_by_id(db=db, submenu_id=submenu_id)
-    _submenu.title = title
-    _submenu.description = description
+def update_menu(db:Session, submenu_id:UUID, data=MenuUpdate):
+    _submenu = get_submenu_by_id(db=db, submenu_id=submenu_id, _data=data)
     db.commit()
     db.refresh(_submenu)
     return _submenu
